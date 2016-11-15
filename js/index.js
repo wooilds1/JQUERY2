4f939e2e2578975fda1710e8522dfcdb48fa4153
wooilds1 <wooilds1@koreatech.ac.kr>
2016-11-15 18:58:35 +0900

    2번째 커밋

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 /**
  * Created by YMH on 2016-11-15.
  */
+
+
+var test1 = 100;
+var test2 = true;
+var test3 = "소리없는 아우성";
+var test4 = null;
+var test5;
+
+console.log(typeof test1);
+console.log(typeof test2);
+console.log(typeof test3);
+console.log(typeof test4);
+console.log(typeof test5);
