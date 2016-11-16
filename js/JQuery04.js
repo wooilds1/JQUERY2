0074d8f7537138190e3a0ccf6ccfa1390933f945
wooilds1 <wooilds1@koreatech.ac.kr>
2016-11-16 20:37:33 +0900

    11.16 commit

---
+++ js/JQuery04.js
@@ -0,0 +1,6 @@
+/**
+ * Created by YMH on 2016-11-16.
+ */
+function myFunc(){
+    alert($("select > option:selected").val());
+}
---
+++ js/JQuery05.js
@@ -0,0 +1,6 @@
+/**
+ * Created by YMH on 2016-11-16.
+ */
+function myFunc(){
+    $("[type=checkbox]:checked").parent().remove();
+}
